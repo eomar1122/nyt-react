f0e5ec9446022af46c7a0e3b007be7fe9a06506d
Elshibani Omar <amr.omar80@gmail.com>
2018-03-25 19:38:29 -0600

    add files

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/nyt-react",
+  MONGODB_URI || "mongodb://localhost/nyt-react",
   {
     useMongoClient: true
   }
